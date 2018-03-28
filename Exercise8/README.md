dfb3519adc7ac539c80e3cc9375b90680c021596
Pawel Drozdz <paweldrozdz.91@interia.pl>
2018-03-28 22:29:49 +0200

    timer.sh is added

--- Exercise8/README.md
+++ Exercise8/README.md
@@ -13,6 +13,11 @@ of the game from 1 to 10:
 	- level 1  --> the numbers generating for expressions are between 1-10
 	- level 10 --> the numbers generating for expressions are between 1-100
 
+### *timer.sh*
+This script counts down from given time (in minutes) and when is counts to zero then it starts to count up.
+It is intended to display on the screen the time left to accomplish some task and then display how much
+time we are late with the task.
+
 ### *if-fi.sh*
 This script represents two way of if-elif-fi statements used in scripting. Except of that, you can find there
 also very frequently used check statements which are used to check if some file is a plain file, script,
---
+++ Exercise8/timer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+MINUTES=$1
+SEC=0
+
+count_down()
+{	 
+	if [ $SEC -eq 0 ]; then
+		MINUTES=$(( MINUTES - 1 ))
+		SEC=60
+	fi
+	SEC=$(( SEC - 1 ))
+	sleep 1
+}
+
+count_up()
+{
+	if [ $SEC -eq 59 ]; then
+		MINUTES=$(( MINUTES + 1 ))
+		SEC=-1
+	fi
+	SEC=$(( SEC + 1 ))
+	sleep 1
+}
+
+display_left_time()
+{
+	printf '%-14s %-2s %-11s %-2s %-10s\n' "You have still" "$MINUTES" "minutes and" "$SEC" "seconds..."
+}
+
+display_late_time()
+{
+	printf '%-14s %-2s %-11s %-2s %-10s\n'  "You are late" "$MINUTES" "minutes and" "$SEC" "seconds"	
+}
+
+while [ $MINUTES -ge 0  ]; do
+	display_left_time
+	count_down
+done
+
+echo Time left!
+SEC=1
+MINUTES=0
+
+while true; do
+	display_late_time
+	count_up
+done
+
